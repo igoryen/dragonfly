1a5ca20cf705bce1f320b27b1a9e8053a801069d
Igor Entaltsev <igor.yentaltsev@gmail.com>
2020-04-28 17:08:14 -0400

    349-b. Ejecting interceptors.

--- src/main.js
+++ src/main.js
@@ -8,15 +8,18 @@ axios.defaults.baseURL = 'https://dragonfly-b227a.firebaseio.com';
 axios.defaults.headers.common['Authorization'] = 'agenerictoken';
 axios.defaults.headers.get['Accepts'] = 'application/json';
 
-axios.interceptors.request.use(config => {
+const req_icr = axios.interceptors.request.use(config => {
   console.log('Req interceptor', config);
   return config;
 });
 
-axios.interceptors.response.use(res => {
+const res_icr = axios.interceptors.response.use(res => {
   console.log('Res interceptor', res);
   return res;
 });
+
+axios.interceptors.request.eject( req_icr );
+axios.interceptors.request.eject( res_icr );
 
 Vue.config.productionTip = false
 
